7118333ae999bbf6f8f3e142d76c7ff02c608c44
hashark <18191362+hashark@users.noreply.github.com>
2019-06-19 18:16:26 +0300

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,1 +1,18 @@
 print 'this is the file we will play with'
+
+
+def make_fun():
+  print 'this function is here'
+  print 'this function is here'
+  print 'this function is here'
+  print 'this function is here'
+  print 'this function is here'
+  print 'this function is here'
+  print 'this function is here'
+  
+def now_what():
+  print 'now WHAT?'
+  print 'now WHAT?'
+  print 'now WHAT?'
+  print 'now WHAT?'
+  print 'now WHAT?'
